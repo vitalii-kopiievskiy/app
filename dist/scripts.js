1e9f595df7e78e8b3889a8a9253f6b5791f3d982
vitalii-kopiievskiy <vitalii.kopiievskiy@gmail.com>
2020-02-12 21:31:33 +0200

    add styles clients

--- dist/scripts.js
+++ dist/scripts.js
@@ -87,3 +87,15 @@ $(function() {
   //   }
   // );
 });
+
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    animateOut: 'fadeOut',
+    items: 1
+  });
+});
